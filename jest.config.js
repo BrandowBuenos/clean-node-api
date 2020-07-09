7ea8610cf53f3d5f8ddd1b37a28c607faa897586
Brandow Buenos <brandowbuenos@gmail.com>
2020-07-09 08:57:55 -0300

    chore: change in project dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/main/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
